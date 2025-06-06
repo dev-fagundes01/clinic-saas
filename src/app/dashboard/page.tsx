2b446cd0136ef18d1df2cf9552e99a9aa1532090
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2025-06-06 15:15:08 -0300

    feat: add authentication guard
    This commit adds authentication guards to both the dashboard and authentication pages.
    
    - The dashboard page now redirects to the authentication page if the user is not authenticated.
    - The authentication page now redirects to the dashboard page if the user is authenticated.
    - Added "use client" to login and sign-up forms.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,23 @@
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import SignOutButton from "./components/sign-out-button";
+import { redirect } from 'next/navigation';
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if(!session?.user) {
+    redirect("/authentication")
+  }
+
   return (
-    <div>page</div>
-  )
+    <div>
+      <h1>Dashboard</h1>
+      <h1>{session?.user?.name}</h1>
+      <h1>{session?.user?.email}</h1>
+      <SignOutButton />
+    </div>
+  );
 }
