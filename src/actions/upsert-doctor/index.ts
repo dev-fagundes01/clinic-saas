9c624f501cbbc223c761ba4cd0c49c55f7fcc4a9
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2025-06-19 20:32:38 -0300

    feat: ensure doctor availability is saved as utc on database

--- src/actions/upsert-doctor/index.ts
+++ src/actions/upsert-doctor/index.ts
@@ -1,33 +1,62 @@
-"use server"
+"use server";
 
-import { auth } from '@/lib/auth';
-import { upsertDoctorSchema } from './schema';
-import { headers } from 'next/headers';
-import { doctorsTable } from '@/db/schema';
-import { db } from '@/db';
-import { actionClient } from '@/lib/safe-actions';
+import { auth } from "@/lib/auth";
+import { upsertDoctorSchema } from "./schema";
+import { headers } from "next/headers";
+import { doctorsTable } from "@/db/schema";
+import { db } from "@/db";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 
-export const upsertDoctor = actionClient.schema(upsertDoctorSchema).action(async ({parsedInput}) => {
-  const session = await auth.api.getSession({
-    headers: await headers()
-  })
+import { actionClient } from "@/lib/safe-actions";
 
-  if (!session?.user) {
-    throw new Error('Unauthorized')
-  }
+dayjs.extend(utc);
 
-  if (!session?.user.clinic?.id) {
-    throw new Error('Clinic not found')
-  }
+export const upsertDoctor = actionClient
+  .schema(upsertDoctorSchema)
+  .action(async ({ parsedInput }) => {
+    const availableFromTime = parsedInput.availableFromTime;
+    const availableToTime = parsedInput.availableToTime;
 
-  await db.insert(doctorsTable).values({
-    id: parsedInput.id,
-    ...parsedInput,
-    clinicId: session?.user.clinic?.id
-  }).onConflictDoUpdate({
-    target: [doctorsTable.id],
-    set: {
-      ...parsedInput
+    const availableFromTimeUTC = dayjs()
+      .set("hour", parseInt(availableFromTime.split(":")[0]))
+      .set("minute", parseInt(availableFromTime.split(":")[1]))
+      .set("second", parseInt(availableFromTime.split(":")[2]))
+      .utc();
+
+    const availableToTimeUTC = dayjs()
+      .set("hour", parseInt(availableToTime.split(":")[0]))
+      .set("minute", parseInt(availableToTime.split(":")[1]))
+      .set("second", parseInt(availableToTime.split(":")[2]))
+      .utc();
+
+    const session = await auth.api.getSession({
+      headers: await headers(),
+    });
+
+    if (!session?.user) {
+      throw new Error("Unauthorized");
     }
-  })
-})
+
+    if (!session?.user.clinic?.id) {
+      throw new Error("Clinic not found");
+    }
+
+    await db
+      .insert(doctorsTable)
+      .values({
+        ...parsedInput,
+        id: parsedInput.id,
+        clinicId: session?.user.clinic?.id,
+        availableFromTime: availableFromTimeUTC.format("HH:mm:ss"),
+        availableToTime: availableToTimeUTC.format("HH:mm:ss"),
+      })
+      .onConflictDoUpdate({
+        target: [doctorsTable.id],
+        set: {
+          ...parsedInput,
+          availableFromTime: availableFromTimeUTC.format("HH:mm:ss"),
+          availableToTime: availableToTimeUTC.format("HH:mm:ss"),
+        },
+      });
+  });
